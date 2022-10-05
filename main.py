c9b51a434d2758ebbc4a0a3176b8554e2820a713
Cristian Castañeda <crisleoco14@gmail.com>
2022-10-04 21:10:54 -0500

    modulo estudiante BD

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# import modules.students as students
+import modules.students as students
 
-# students.print_message()
 
-from modules.students import print_message
-print_message()
+def readDataUser():
+    id = input("Enter the id of the student: ")
+    id = id.ljust(12)  # Fix to the left 12 positions
+    name = input("Enter the name of the student: ")
+    last_name = input("Enter the last_name of the student: ")
+    career = input("Enter the career of the student: ")
+    born_date = input("Enter the born_date of the student: ")
+    entry_date = input("Enter the entry_date of the student: ")
+    place_origin = input("Enter the place_origin of the student: ")
+    email = input("Enter the email of the student: ")
+    enrol_quantity = input("Enter the enrol_quantity of the student: ")
+    photograph = input("Enter the photograph of the student: ")
+    student = (id, name, last_name, career, born_date, entry_date,
+               place_origin, email, enrol_quantity, photograph)
+    return student
+
+
+def main():
+    my_connection = students.connectionToDB()
+    students.createTableStudent(my_connection)
+    # readDataUser()
+
+
+if __name__ == "__main__":
+    main()
--- modules/students.py
+++ modules/students.py
@@ -1,2 +1,28 @@
-def print_message():
-    print("Hello from students.py")
+import sqlite3
+from sqlite3 import Error
+
+
+def connectionToDB():
+    try:
+        connection = sqlite3.connect(
+            './res/database.db')  # Database connection
+        return connection
+    except Error:
+        print(Error)
+
+
+def createTableStudent(connection):
+    cursor_obj = connection.cursor()
+    id = "code INTEGER PRIMARY KEY"
+    name = "name TEXT"
+    last_name = "lastname TEXT"
+    career = "career TEXT"
+    born_date = "bornDate TEXT"
+    entry_date = "entryDate TEXT"
+    place_origin = "placeOrigin TEXT"
+    email = "email TEXT"
+    enroll_quantity = "enrolQuantity INTEGER"
+    photograph = "photograph TEXT"
+    create_statement = f"CREATE TABLE IF NOT EXISTS students({id}, {name}, {last_name}, {career}, {born_date}, {entry_date}, {place_origin}, {email}, {enroll_quantity}, {photograph})"
+    cursor_obj.execute(create_statement)
+    connection.commit()  # Save DB
