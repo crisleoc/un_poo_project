61cf8bd463db48407bb0f3e39b6f1c93e7f594fb
Miguel Ochoa <miochoam@unal.edu.co>
2022-10-18 20:34:29 -0500

    ads

--- modules/subjects.py
+++ modules/subjects.py
@@ -1,13 +1,49 @@
 def createTableSubjects(connection):
+    
+    """Create the Subjects table in the database:
+
+    Args:
+        connection (object): Connection to sqlite3.
+    """
+    
     cursor_obj = connection.cursor()
-    codigo = "Código INTEGER PRIMARY KEY NOT NULL UNIQUE"
-    nombre = "Nombre TEXT NOT NULL UNIQUE"
-    facu = "Dacultad TEXT"
-    dep = "Departamento TEXT"
-    cred = "Créditos TEXT"
-    idioma = "Idioma TEXT"
-    create_statement = f"""CREATE TABLE IF NOT EXISTS subjects(
-        {codigo}, {nombre}, {facu}, {dep}, {cred}, {idioma}
+    code = "Code INTEGER PRIMARY KEY NOT NULL UNIQUE"
+    name = "Name TEXT NOT NULL UNIQUE"
+    school = "School TEXT"
+    department = "Department TEXT"
+    credits = "Credits TEXT"
+    language = "Language TEXT"
+    create_statement = f"""CREATE TABLE IF NOT EXISTS Subjects(
+        {code}, {name}, {school}, {department}, {credits}, {language}
         )"""
     cursor_obj.execute(create_statement)
     connection.commit()  # Save DB
+
+
+def insertSubject(connection, subject):
+    """Insert a subject in the Subjects table:
+
+    Args:
+        connection (object): Connection to sqlite3.
+        subject (tuple): Tuple containing the elements of the Subjects table.
+        
+    """
+    cursor_obj = connection.cursor()
+    create_statement = "INSERT INTO subject VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    cursor_obj.execute(create_statement, subject)
+    connection.commit()
+
+
+def selectAllSubjects(connection):
+    """Select all of the subjects in the Subjects table:
+
+    Args:
+        connection (object): Connection to sqlite3.
+
+    Returns:
+        list<tuple>: List of tuples with the data of the subjects.
+    """
+    cursor_obj = connection.cursor()
+    create_statement = "SELECT * FROM Subjects"
+    cursor_obj.execute(create_statement)
+    return cursor_obj.fetchall()
