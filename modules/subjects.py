3eabed90ac5eff04aa6242049fb3116aabc30879
Cristian Castañeda <crisleoco14@gmail.com>
2022-10-18 22:42:25 -0500

    Pequeñas correcciones

--- modules/subjects.py
+++ modules/subjects.py
@@ -1,19 +1,18 @@
 def createTableSubjects(connection):
-    
-    """Create the Subjects table in the database:
+    """Create the subjects table in the database:
 
     Args:
         connection (object): Connection to sqlite3.
     """
-    
+
     cursor_obj = connection.cursor()
-    code = "Code INTEGER PRIMARY KEY NOT NULL UNIQUE"
-    name = "Name TEXT NOT NULL UNIQUE"
-    school = "School TEXT"
-    department = "Department TEXT"
-    credits = "Credits TEXT"
-    language = "Language TEXT"
-    create_statement = f"""CREATE TABLE IF NOT EXISTS Subjects(
+    code = "code INTEGER PRIMARY KEY NOT NULL UNIQUE"
+    name = "name TEXT NOT NULL UNIQUE"
+    school = "school TEXT"
+    department = "department TEXT"
+    credits = "credits TEXT"
+    language = "language TEXT"
+    create_statement = f"""CREATE TABLE IF NOT EXISTS subjects(
         {code}, {name}, {school}, {department}, {credits}, {language}
         )"""
     cursor_obj.execute(create_statement)
@@ -21,13 +20,12 @@ def createTableSubjects(connection):
 
 
 def insertSubject(connection, subject):
-    
-    """Insert a subject in the Subjects table:
+    """Insert a subject in the subjects table:
 
     Args:
         connection (object): Connection to sqlite3.
         subject (tuple): Tuple containing the elements of the Subjects table.
-        
+
     """
     cursor_obj = connection.cursor()
     create_statement = "INSERT INTO subject VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
@@ -36,7 +34,6 @@ def insertSubject(connection, subject):
 
 
 def selectAllSubjects(connection):
-    
     """Select all of the subjects in the Subjects table:
 
     Args:
@@ -46,6 +43,6 @@ def selectAllSubjects(connection):
         list<tuple>: List of tuples with the data of the subjects.
     """
     cursor_obj = connection.cursor()
-    create_statement = "SELECT * FROM Subjects"
+    create_statement = "SELECT * FROM subjects"
     cursor_obj.execute(create_statement)
     return cursor_obj.fetchall()
