899f71c921c2b1485029e95dfeced32eace962b6
Miguel Ochoa <miochoam@unal.edu.co>
2022-11-15 22:48:57 -0500

    a

---
+++ modules/object.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+class object():
+    
+    @abstractmethod
+    def __init__(self): pass
+    
+    @abstractmethod
+    def queryObject(self): pass
+    
+    @abstractmethod
+    def eraseObject(self): pass
+    
+    @abstractmethod
+    def updateObject(self): pass
+    
+    
+        
+    
